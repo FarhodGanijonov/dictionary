bc7f2da583c066505f1a632b26e2fa1ad9a49c93
FarhodGanijonov <farhodganijonov81@gmail.com>
2024-08-24 17:24:59 +0500

    13

--- config/settings.py
+++ config/settings.py
@@ -157,35 +157,28 @@ CKEDITOR_CONFIGS = {
 }
 
 
-JAZZMIN_UI_TWEAKS = {
-    "navbar_small_text": False,
-    "footer_small_text": False,
-    "body_small_text": False,
-    "brand_small_text": False,
-    "brand_colour": False,
-    "accent": "accent-primary",
-    "navbar": "navbar-white navbar-light",
-    "no_navbar_border": False,
-    "navbar_fixed": True,
-    "layout_boxed": False,
-    "footer_fixed": False,
-    "sidebar_fixed": False,
-    "sidebar": "sidebar-dark-primary",
-    "sidebar_nav_small_text": False,
-    "sidebar_disable_expand": False,
-    "sidebar_nav_child_indent": False,
-    "sidebar_nav_compact_style": False,
-    "sidebar_nav_legacy_style": False,
-    "sidebar_nav_flat_style": True,
-    "theme": "default",
-    "dark_mode_theme": None,
-    "button_classes": {
-        "primary": "btn-primary",
-        "secondary": "btn-secondary",
-        "info": "btn-info",
-        "warning": "btn-warning",
-        "danger": "btn-danger",
-        "success": "btn-success"
+JAZZMIN_SETTINGS = {
+    "site_title": "My Site Admin",
+    "site_header": "My Site Administration",
+    "site_brand": "My Site",
+    "welcome_sign": "Welcome to My Site Admin",
+    "copyright": "My Site",
+    "search_model": ["auth.User"],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "show_ui_builder": True,
+    "custom_links": {
+        "auth": [
+            {
+                "name": "Users",
+                "url": "admin:auth_user_changelist",
+                "permissions": ["auth.view_user"],
+            },
+            {
+                "name": "Groups",
+                "url": "admin:auth_group_changelist",
+                "permissions": ["auth.view_group"],
+            },
+        ],
     },
-    "actions_sticky_top": False
 }
