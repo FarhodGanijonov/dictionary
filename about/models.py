d858cc2d82018fe4fcfaf0d139adfaa33fac14c3
FarhodGanijonov <farhodganijonov81@gmail.com>
2024-11-10 11:47:39 +0500

    blank

--- about/models.py
+++ about/models.py
@@ -24,7 +24,7 @@ class Scientists(models.Model):
 
 
 class NewsCategory(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
