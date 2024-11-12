d0d9e276837e162faa0107e6a4952b07b951720e
FarhodGanijonov <farhodganijonov81@gmail.com>
2024-11-12 21:06:15 +0500

    blank

--- about/models.py
+++ about/models.py
@@ -72,10 +72,10 @@ class Sentences(models.Model):
 class AdminContact(models.Model):
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
-    location = models.TextField()
-    telegram = models.URLField()
-    instagram = models.URLField(default='')
-    youtube = models.URLField()
+    location = models.TextField(blank=True, null=True)
+    telegram = models.URLField(default='', blank=True, null=True)
+    instagram = models.URLField(default='', blank=True, null=True)
+    youtube = models.URLField(default='', blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if AdminContact.objects.exists() and not self.pk:
